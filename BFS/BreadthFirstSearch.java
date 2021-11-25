00d4ee67d38792eb7b5422443f96afafe8f842b6
NahianShabab <nsnbd134@gmail.com>
2021-11-26 00:36:55 +0600

    Added Breadth First Search Functionality

---
+++ BFS/BreadthFirstSearch.java
@@ -0,0 +1,39 @@
+package BFS;
+
+import java.util.LinkedList;
+import java.util.List;
+
+import Graph.Graph;
+
+public class BreadthFirstSearch {
+    public enum VertexColor{
+         WHITE,BLACK,GREY
+    };
+
+    public static void Search(Graph g,int s,int [] pred, VertexColor [] color,int dist[]){
+        int vertices=g.vertices;
+        for(int i=0;i<vertices;i++){
+            pred[i]=-1;
+            color[i]=VertexColor.WHITE;
+            dist[i]=-1;
+        }
+        LinkedList<Integer> queue=new LinkedList<Integer>();
+        color[s]=VertexColor.GREY;
+        dist[s]=0;
+        queue.addLast(s);
+        while(!queue.isEmpty()){
+            int u=queue.removeFirst();
+            List<Integer> AList=g.adjacencyLists[u];
+            for(int i=0;i<AList.size();i++){
+                if(color[AList.get(i)]==VertexColor.WHITE){
+                    color[AList.get(i)]=VertexColor.GREY;
+                    pred[AList.get(i)]=u;
+                    dist[AList.get(i)]=dist[u]+1;
+                    queue.addLast(AList.get(i));
+                }
+            }
+            color[u]=VertexColor.BLACK;
+        }
+    }
+
+}
--- Main.java
+++ Main.java
@@ -1,6 +1,26 @@
 
+import BFS.BreadthFirstSearch;
+import BFS.BreadthFirstSearch.VertexColor;
+import Graph.Graph;
+import Graph.UndirectedGraph;
+
 public class Main{
     public static void main(String[] args) {
+        UndirectedGraph g=new UndirectedGraph(8);
+        g.addEdge(0, 1);
+        g.addEdge(0, 3);
+        g.addEdge(1, 2);
+        g.addEdge(2, 3);
+        g.addEdge(3, 4);
+        g.addEdge(4, 5);
+        g.addEdge(5, 6);
+        int [] dist=new int[g.vertices];
+        VertexColor [] color=new VertexColor[g.vertices];
+        int [] pred=new int [g.vertices];
+        int source=3;
+        BreadthFirstSearch.Search(g, source, pred, color, dist);
 
+        for(int i=0;i<g.vertices;i++)
+            System.out.println(dist[i]);
     }
 }

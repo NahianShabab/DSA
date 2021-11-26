ed357c328740c6effdf32fb5dd38afd9fce6d955
NahianShabab <nsnbd134@gmail.com>
2021-11-26 19:36:11 +0600

    Modified DFS Search so that it prints all sorts of edges ( Tree,Back,Cross,Forward)

--- DFS/DepthSearch.java
+++ DFS/DepthSearch.java
@@ -8,6 +8,7 @@ public class DepthSearch {
     public enum VertexColor {
         WHITE,GREY,BLACK
     };
+    private static int time=0;
     public static void Search(Graph g,int pred[],int [] discovered,int [] finished){
         int vertices=g.vertices;
         VertexColor [] color=new VertexColor[vertices];
@@ -15,15 +16,15 @@ public class DepthSearch {
             pred[i]=-1;
             color[i]=VertexColor.WHITE;
         }
-        int time=0;
+        time=0;
         for(int i=0;i<vertices;i++){
             if(color[i]==VertexColor.WHITE){
-                DepthSearchVisit(g,i,pred, discovered, finished, time, color);
+                DepthSearchVisit(g,i,pred, discovered, finished, color);
             }
         }
 
     }
-    private static void DepthSearchVisit(Graph g,int u,int pred[],int [] discovered,int [] finished,int time,VertexColor [] color){
+    private static void DepthSearchVisit(Graph g,int u,int pred[],int [] discovered,int [] finished,VertexColor [] color){
         time+=1;
         discovered[u]=time;
         color[u]=VertexColor.GREY;
@@ -32,7 +33,19 @@ public class DepthSearch {
             int v=AList.get(i);
             if(color[v]==VertexColor.WHITE){
                 pred[v]=u;
-                DepthSearchVisit(g, v, pred, discovered, finished, time, color);
+                System.out.println("Tree: ("+u+","+v+")");
+                DepthSearchVisit(g, v, pred, discovered, finished, color);
+            }
+            else if(color[v]==VertexColor.GREY){
+                System.out.println("Back: ("+u+","+v+")");
+            }
+            else if(color[v]==VertexColor.BLACK){
+                if(discovered[u]<discovered[v]){
+                    System.out.println("Forward: ("+u+","+v+")");
+                }
+                else{
+                    System.out.println("Cross: ("+u+","+v+")");
+                }
             }
         }
         color[u]=VertexColor.BLACK;
